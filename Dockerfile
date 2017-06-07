7babddf7378846865aed0e397d898dd3e92e3a6f
Steve Elliott <steve@tegud.net>
2017-06-07 19:25:06 +0100

    basic auth

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
 FROM nginx:stable
 
 COPY nginx.conf /etc/nginx/conf.d/default.conf
+COPY run.sh /
+
+ENTRYPOINT ["/run.sh"]
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+if [ -z $BASIC_AUTH_USERNAME ]; then
+  echo >&2 "BASIC_AUTH_USERNAME must be set"
+  exit 1
+fi
+
+if [ -z $BASIC_AUTH_PASSWORD ]; then
+  echo >&2 "BASIC_AUTH_PASSWORD must be set"
+  exit 1
+fi
+
+htpasswd -bBc /etc/nginx/.htpasswd $BASIC_AUTH_USERNAME $BASIC_AUTH_PASSWORD
+
+nginx -g "daemon off;"
